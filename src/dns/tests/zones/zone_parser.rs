353e6d51ec9cf283a795058e7a62f39732aec0b9
onihilist <mashirooo.yt@gmail.com>
2025-12-16 17:40:50 +0100

    fix(zone_parser): change the arg and expected in test_zone_parser()

--- src/dns/tests/zones/zone_parser.rs
+++ src/dns/tests/zones/zone_parser.rs
@@ -7,14 +7,14 @@ mod tests {
     #[test]
     fn test_zone_parser() {
         let expected_zone: Zone = Zone {
-            origin: Some("onihilist.moe".to_string()),
+            origin: Some("nihilist.moe.".to_string()),
             name: "".to_string(),
             ttl: 0,
             soa: None,
             records: Default::default(),
         };
 
-        let result = zone_parser("onihilist.moe");
+        let result = zone_parser("nihilist.moe");
 
         //assert_eq!(result.is_ok(), true);
         assert_eq!(expected_zone, result.unwrap());
